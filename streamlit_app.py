cd85235a8ef031a43b78c6d2f1bfcc5b424c287b
ptmy <135793759+ptmy@users.noreply.github.com>
2023-06-07 06:18:25 +0200

    Update streamlit_app.py
    add pandas and fruitlist in s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard -boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.text('🍌🥭Build Your Own Fruit Smoothie  🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
